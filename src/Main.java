8ce7ea6e07c5f2cf4818c8951b7cfb9757841f37
kody <w.jamesdevera@gmail.com>
2024-11-20 12:06:21 +0800

    added welcome message

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,36 @@
 import cpu.scheduling.ShortestJobFirst;
 import utils.Process;
+import java.util.Scanner;
 
-import java.util.ArrayList;
 
 public class Main {
+    private static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        ArrayList<Process> processes = new ArrayList<>();
-        processes.add(new Process(1, 3, 4));
-        processes.add(new Process(2, 5, 9));
-        processes.add(new Process(3, 8, 4));
-        processes.add(new Process(4, 0, 7));
-        processes.add(new Process(5, 12, 6));
-        ShortestJobFirst sjf = new ShortestJobFirst(processes);
-        sjf.simulate();
-        ArrayList<Process> completedProcess = sjf.getCompletedProcess();
-        for (Process process: completedProcess) {
-            System.out.println(process);
-            System.out.println(process.getCompletionTime());
-            System.out.println(process.getTurnAroundTime());
-            System.out.println(process.getWaitingTime());
-            System.out.println("\n");
-        }
+        printWelcomeMessage();
+        String input = requestUserInput();
+    }
+
+    private static void printWelcomeMessage() {
+        System.out.println("\u001B[35m\n   ____    _____           ___     __    ______   ____");
+        System.out.println("  / __ \\  / ___/          /   |   / /   / ____/  / __ \\");
+        System.out.println(" / / / /  \\__ \\          / /| |  / /   / / __   / / / /");
+        System.out.println("/ /_/ /  ___/ /         / ___ | / /___/ /_/ /  / /_/ /");
+        System.out.println("\\____/  /____/         /_/  |_|/_____/\\____/   \\____/\u001B[37m");
+        System.out.println("==============================================================");
+        System.out.println("  Welcome to the CPU Scheduling Simulator v1.0");
+        System.out.println();
+        System.out.println("  Supported Scheduling Algorithms:");
+        System.out.println("    🔹 Shortest Job First (SJF)");
+        System.out.println("    🔹 Round Robin (RR)");
+        System.out.println("    🔹 SCAN Disk Scheduling");
+        System.out.println();
+        System.out.println("  🚀 Let's get started! Type 'help' for available commands.");
+        System.out.println("  🛑 Type 'exit' at any time to quit the application.");
+        System.out.println("==============================================================\n");
+    }
+
+    private static String requestUserInput() {
+        System.out.print(":");
+        return scan.nextLine();
     }
 }
